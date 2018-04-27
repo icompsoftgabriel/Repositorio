eba20424b8ff0824b9abbf39fc90c098159aab1d
gabriel <gabriel.casaril@icompsoft.com.br>
2018-04-27 10:30:15 -0300

    atualizcao classes e banco

--- banco-produto.php
+++ banco-produto.php
@@ -7,8 +7,22 @@ function listaProdutos($conexao) {
 	$resultado = mysqli_query($conexao, "select p.*,c.nome as categoria_nome 
 		from produtos as p join categorias as c on c.id=p.categoria_id");
 
-	while($produto = mysqli_fetch_assoc($resultado)) {
+	while($produto_array = mysqli_fetch_assoc($resultado)) {
+		
+
+		$categoria = new Categoria();
+		$categoria->nome = $produto_array['categoria_nome'];
+
+		$produto = new Produto();
+		$produto->id = $produto_array ['id'];
+		$produto->nome = $produto_array ['nome'];
+		$produto->descricao = $produto_array ['descricao'];
+		$produto->categoria = $categoria;
+		$produto->preco = $produto_array ['preco'];
+		$produto->usado = $produto_array ['usado'];]
+
 		array_push($produtos, $produto);
+
 	}
 
 	return $produtos;
---
+++ class/Categoria.php
@@ -0,0 +1,15 @@
+<?php
+
+	class Categoria{
+
+		public $id;
+
+		public $nome;
+
+
+
+	}
+	
+
+
+?>
