dbf321b34402845e649cc0d9e31b885243c38d12
gabriel <gabriel.casaril@icompsoft.com.br>
2018-04-27 15:33:33 -0300

    atualizado setters e getters

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -8,28 +8,28 @@ require_once("class/Categoria.php");
 verificaUsuario();
 
 $categoria = new Categoria();
-$categoria->id = $_POST['categoria_id'];
+$categoria->setId($_POST['categoria_id']);
 
 $produto = new Produto();
-$produto->nome = $_POST['nome'];
-$produto->preco = $_POST['preco'];
-$produto->descricao = $_POST['descricao'];
+$produto->setNome($_POST['nome']);
+$produto->setPreco($_POST['preco']);
+$produto->setDescricao($_POST['descricao']);
 
 if(array_key_exists('usado', $_POST)) {
-	$produto->usado = "true";
+	$produto->setUsado("true");
 } else {
-	$produto->usado = "false";
+	$produto->setUsado("false");
 }
 
-$produto->categoria = $categoria;
+$produto->setCategoria($categoria);
 
 if(insereProduto($conexao, $produto)) { ?>
-	<p class="text-success">O produto <?= $produto->nome ?>, <?= $produto->preco ?> foi adicionado.</p>
+	<p class="text-success">O produto <?= $produto->getNome() ?>, <?= $produto->getPreco() ?> foi adicionado.</p>
 <?php 
 } else {
 	$msg = mysqli_error($conexao);
 ?>
-	<p class="text-danger">O produto <?= $produto->nome ?> não foi adicionado: <?= $msg?></p>
+	<p class="text-danger">O produto <?= $produto->getNome() ?> não foi adicionado: <?= $msg?></p>
 <?php
 }
 ?>
--- banco-produto.php
+++ banco-produto.php
@@ -12,15 +12,15 @@ function listaProdutos($conexao) {
 	while($produto_array = mysqli_fetch_assoc($resultado)) {
 
 		$categoria = new Categoria();
-		$categoria->nome = $produto_array['categoria_nome'];
+		$categoria->setNome($produto_array['categoria_nome']);
 
 		$produto = new Produto();
-		$produto->id = $produto_array['id'];
-		$produto->nome = $produto_array['nome'];
-		$produto->descricao = $produto_array['descricao'];
-		$produto->categoria = $categoria;
-		$produto->preco = $produto_array['preco'];
-		$produto->usado = $produto_array['usado'];
+		$produto->setId($produto_array['id']);
+		$produto->setNome($produto_array['nome']);
+		$produto->setDescricao($produto_array['descricao']);
+		$produto->setCategoria($categoria);
+		$produto->setPreco($produto_array['preco']);
+		$produto->setUsado($produto_array['usado']);
 
 		array_push($produtos, $produto);
 	}
@@ -31,18 +31,19 @@ function listaProdutos($conexao) {
 function insereProduto($conexao, Produto $produto) {
 
 	$query = "insert into produtos (nome, preco, descricao, categoria_id, usado) 
-		values ('{$produto->nome}', {$produto->preco}, '{$produto->descricao}', 
-			{$produto->categoria->id}, {$produto->usado})";
+		values ('{$produto->getNome()}', {$produto->getPreco()}, 
+			'{$produto->getDescricao()}', {$produto->getCategoria()->getId()}, 
+				{$produto->isUsado()})";
 
 	return mysqli_query($conexao, $query);
 }
 
 function alteraProduto($conexao, Produto $produto) {
 
-	$query = "update produtos set nome = '{$produto->nome}', 
-		preco = {$produto->preco}, descricao = '{$produto->descricao}', 
-			categoria_id= {$produto->categoria->id}, usado = {$produto->usado} 
-				where id = '{$produto->id}'";
+	$query = "update produtos set nome = '{$produto->getNome()}', 
+		preco = {$produto->getPreco()}, descricao = '{$produto->getDescricao()}', 
+			categoria_id= {$produto->getCategoria()->getId()}, 
+				usado = {$produto->isUsado()} where id = '{$produto->getId()}'";
 
 	return mysqli_query($conexao, $query);
 }
@@ -54,15 +55,15 @@ function buscaProduto($conexao, $id) {
 	$produto_buscado = mysqli_fetch_assoc($resultado);
 
 	$categoria = new Categoria();
-	$categoria->id = $produto_buscado['categoria_id'];
+	$categoria->setId($produto_buscado['categoria_id']);
 
 	$produto = new Produto();
-	$produto->id = $produto_buscado['id'];
-	$produto->nome = $produto_buscado['nome'];
-	$produto->descricao = $produto_buscado['descricao'];
-	$produto->categoria = $categoria;
-	$produto->preco = $produto_buscado['preco'];
-	$produto->usado = $produto_buscado['usado'];
+	$produto->setId($produto_buscado['id']);
+	$produto->setNome($produto_buscado['nome']);
+	$produto->setDescricao($produto_buscado['descricao']);
+	$produto->setCategoria($categoria);
+	$produto->setPreco($produto_buscado['preco']);
+	$produto->setUsado($produto_buscado['usado']);
 
 	return $produto;
 }
